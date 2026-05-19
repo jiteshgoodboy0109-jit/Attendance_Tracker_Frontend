01d2d68fd24d7140707c1658a5050a931a5e0a1c
DHARANESH001 <dharaneshv001@gmail.com>
2026-05-19 14:29:19 +0530

    Structure updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './components/Login/Login'
-import ForgotPassword from './components/Login/ForgotPassword'
+import Login from './Pages/auth/Login'
+import ForgotPassword from './Pages/auth/ForgotPassword'
 
 import './index.css'
 
